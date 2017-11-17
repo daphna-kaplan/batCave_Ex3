34d48b6ec5a46255dc7768fb18d987fc76e463b9
Kaplan Daphna <kaplan.daphna@gmail.com>
2017-11-17 13:12:01 +0200

    Add files via upload

---
+++ Assets/Scripts/batcave/BatKeyboardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BatCave;
+using Infra.Gameplay.UI;
+using UnityEngine.EventSystems;
+
+[CreateAssetMenu(menuName = "Bat Controller/Keyboard")]
+public class BatKeyboardController : BatController
+{
+    override public bool WantsToFlyUp()
+    {
+        // Handle keyboard input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            return true;
+        }
+        else if (!Input.GetKeyDown(KeyCode.Space))
+        {
+
+            return false;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/batcave/BatMouseController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BatCave;
+using Infra.Gameplay.UI;
+using UnityEngine.EventSystems;
+
+[CreateAssetMenu(menuName = "Bat Controller/Mouse")]
+public class BatMouseController : BatController
+{
+
+    private bool flyUp;
+
+    private void OnEnable()
+    {
+        GameInputCapture.OnTouchDown += OnTouchDown;
+        GameInputCapture.OnTouchUp += OnTouchUp;
+
+    }
+
+    private void OnDisable()
+    {
+        GameInputCapture.OnTouchDown -= OnTouchDown;
+        GameInputCapture.OnTouchUp -= OnTouchUp;
+
+    }
+
+    private void OnTouchDown(PointerEventData e)
+    {
+        flyUp = true;
+    }
+
+    private void OnTouchUp(PointerEventData e)
+    {
+        flyUp = false;
+    }
+
+
+override public bool WantsToFlyUp()
+    {
+        return flyUp;
+    }
+}
